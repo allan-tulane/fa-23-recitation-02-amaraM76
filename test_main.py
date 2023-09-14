b1e253dcd34acd922f915c131674e246504be3c1
amaraM76 <136095008+amaraM76@users.noreply.github.com>
2023-09-14 00:31:12 -0500

    lab2

--- test_main.py
+++ test_main.py
@@ -2,11 +2,14 @@ from main import *
 
 def test_simple_work():
 	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert simple_work_calc(10, 2, 2) == 286
+	assert simple_work_calc(20, 3, 2) == 366
+	assert simple_work_calc(30, 4, 2) == 552
+	assert simple_work_calc(1, 2, 2) == 1  # Additional test with n=1
+	assert simple_work_calc(5, 2, 2) == 43 
+
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  assert work_calc(10, 2, 2,lambda n: 1) == 21
+  assert work_calc(20, 1, 2, lambda n: n*n) == 2870
+  assert work_calc(30, 3, 2, lambda n: n) == 961
